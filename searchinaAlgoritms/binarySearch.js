08fcc88ae3e0e2639bdfd14c96b0d3ab13e050bf
Abid Khan <abid.khan@kivihealth.com>
2022-11-02 12:11:43 +0530

    indentation fix

--- searchinaAlgoritms/binarySearch.js
+++ searchinaAlgoritms/binarySearch.js
@@ -1,25 +1,24 @@
 function binarySearch(arr, num){
 	let low = 0;
-  let high = arr.length - 1;
-  while(low <= high){
-  	let mid = Math.floor((low + high) / 2);
-    if(arr[mid] == num) {
-    
-    	return mid;
-      
+    let high = arr.length - 1;
+    while(low <= high){
+        let mid = Math.floor((low + high) / 2);
+        if(arr[mid] == num) {
+        
+            return mid;
+        }
+        
+        if(arr[mid] > num){
+        
+            high = mid - 1
+        
+        }else{
+        
+            low = mid + 1
+        
+        }
     }
-    
-    if(arr[mid] > num){
-    
-    	high = mid - 1
-      
-    }else{
-    
-    	low = mid + 1
-      
-    }
-  }
-  return -1
+    return -1
 }
 
 
