f0635e05ab36d72b0cda783e3ddccf3c4d3ebb24
Abid Khan <abid.khan@kivihealth.com>
2022-11-02 12:12:52 +0530

    updated

---
+++ searchingAlgoritms/binarySearch.js
@@ -0,0 +1,28 @@
+function binarySearch(arr, num){
+	let low = 0;
+    let high = arr.length - 1;
+    while(low <= high){
+        let mid = Math.floor((low + high) / 2);
+        if(arr[mid] == num) {
+        
+            return mid;
+        }
+        
+        if(arr[mid] > num){
+        
+            high = mid - 1
+        
+        }else{
+        
+            low = mid + 1
+        
+        }
+    }
+    return -1
+}
+
+
+
+// - [0, 1, 2, 3, 4, 5, 7, 8, 9]
+
+console.log(binarySearch([0, 1, 2, 3, 4, 5, 7, 8, 9], 7));
