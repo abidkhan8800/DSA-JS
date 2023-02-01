85460d90935b6b3928c591c49322b28855444633
Abid Khan <abid.khan@kivihealth.com>
2023-02-01 15:28:00 +0530

    added floyd cycle detection method

--- arrays/basics/findDuplicate.js
+++ arrays/basics/findDuplicate.js
@@ -30,7 +30,44 @@ function findDuplicate2(nums){
     }
 }
 
-console.log("Actual: ",findDuplicate([1,3,4,2,2]), "Expected",2, findDuplicate([1,3,4,2,2]) == 2 ? "PASSED" : "FAILED", "\n")
+console.log("Actual: ",findDuplicate2([1,3,4,2,2]), "Expected",2, findDuplicate2([1,3,4,2,2]) == 2 ? "PASSED" : "FAILED", "\n")
 
 
+// 1 Using Object
 
+console.log("Approach 2")
+function findDuplicate3(nums){
+    let obj = {};
+    for(let item of nums){
+        if(obj[item]){
+            return item
+        }else{
+            obj[item] = 1
+        }
+    }
+}
+
+// console.log("Actual: ",findDuplicate3([1,3,4,2,2]), "Expected",2, findDuplicate3([1,3,4,2,2]) == 2 ? "PASSED" : "FAILED", "\n")
+
+
+function findDuplicate4(nums){
+    let slow = nums[0];
+    let fast = nums[0];
+
+    while(true){
+        slow = nums[slow];
+        fast = nums[nums[fast]]
+        if(slow == fast) break;
+    }
+    console.log(slow, fast)
+     slow = nums[0];
+    while(slow != fast){
+
+        slow = nums[slow]
+        fast = nums[fast]
+    }
+
+    return fast
+}
+
+console.log("Actual: ",findDuplicate4([2, 6, 4, 1, 3, 1, 5]), "Expected",1)
